01c04c5f25d2a864a5bfbd299f95f0b75d332f6a
Veronika_Berezhnaia <berezhnaiav@gmail.com>
2022-02-18 03:38:55 +0100

    req1

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ model = pickle.load(open('saved_model.pkl','rb'))
 def predict():
     try:
         data = request.get_json()
-        print("print: "+str([data["data"]][0]))
-        repr("repr: "+str([data["data"]][0]))
-        prediction = model.predict_proba(data["data"][0])
+        print("print: "+str(pd.DataFrame(data["data"], columns=data["columns"]))
+ #       prediction = model.predict_proba(data["data"][0])
+        prediction = model.predict_proba(pd.DataFrame(data["data"], columns=data["columns"])
         print(prediction)
         output = {'predictions': prediction.tolist()[0]}
         return jsonify( output )
