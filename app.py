6bcbe740f4959246fcbcaed06a85f1e242b37cc3
Veronika_Berezhnaia <berezhnaiav@gmail.com>
2022-02-18 04:02:36 +0100

    test

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ model = pickle.load(open('saved_model.pkl','rb'))
 def predict():
     try:
         data = request.get_json()
- #      df=pandas.DataFrame(data["data"], columns=data["columns"])
+       df=pandas.DataFrame(data["data"], columns=data["columns"])
  #       print("print: "+str(df))
- #       prediction = model.predict_proba(df)
-        prediction = model.predict_proba(pandas.DataFrame(data["data"], columns=data["columns"]))
+        prediction = model.predict_proba(df)
+ #       prediction = model.predict_proba(pandas.DataFrame(data["data"], columns=data["columns"]))
         print(prediction)
- #       output = {'predictions': prediction.tolist()[0]}
-        output = {'predictions': prediction[0]}
+        output = {'predictions': prediction.tolist()[0][0]}
+ #       output = {'predictions': prediction[0]}
         return jsonify( output )
     except NameError:
         print(NameError)
