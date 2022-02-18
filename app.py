e63f06d66259a7688d8e6195b14f875cfe23353d
Veronika_Berezhnaia <berezhnaiav@gmail.com>
2022-02-18 02:41:42 +0100

    req12

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import pickle
 
+import sklearn
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
